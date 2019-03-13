352f91d15a789f64b1d5371c61f2bb06a9e38ea2
Catharina Komrij <komcath@gmail.com>
2019-03-12 20:18:18 -0600

    rails first task project

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # shows the list of tasks
+  get 'tasks', to: 'tasks#index'
+  # creates a new task
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+  # shows one task
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  # updates the task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
+  patch 'tasks/:id', to: 'tasks#update'
+  # delete the task
+  delete "tasks/:id", to: "tasks#destroy", as: :destroy
+
+  # root url
+  root 'tasks#index'
 end
